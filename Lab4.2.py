22a33a23790444d5d3d6d4976288b6beceda3cb5
AnnaZamriy <annazamriy26@gmail.com>
2024-09-22 12:23:49 +0300

    ready

---
+++ Lab4.2.py
@@ -0,0 +1,7 @@
+from math import *
+x = float(input("Введіть значення х:"))
+y = float(input("Введіть значення y:"))
+
+s = cos(x) * tan(y) + log(exp(x) + 4, 5) + (1 / (sqrt(abs(x) + 0.1)))
+
+print(s)
